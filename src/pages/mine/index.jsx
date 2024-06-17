c210bbd8b2614964a042ea54abf70b20bf62a935
duruo <duruo@dianjia.io>
2024-06-17 15:35:22 +0800

    feat:完成我的页面

--- src/pages/mine/index.jsx
+++ src/pages/mine/index.jsx
@@ -1,6 +1,33 @@
-import { View } from '@tarojs/components'
+import { View } from "@tarojs/components";
+import "./index.scss";
+import {
+  AtCard as Card,
+  AtAvatar as Avatar,
+  AtList,
+  AtListItem,
+} from "taro-ui";
 const App = () => {
-    return <View>mine</View>
-}
+  return (
+    <View className="mine-container">
+      <View className="mine-card">
+        <View className="mine-header">
+          <Avatar circle />
+          <View style={{ marginLeft: "20px" }}>
+            <View style={{ fontSize: "20px" }}>thomas</View>
+            <View style={{ fontSize: "10px" }}>LV:1初级</View>
+          </View>
+        </View>
+        
+      </View>
+      <View className="mine-content">
+          <AtList>
+            <AtListItem title="我的旅行计划" arrow="right"  />
+            <AtListItem title="免责声明" arrow="right" />
+            <AtListItem title="联系我们" arrow="right" />
+          </AtList>
+        </View>
+    </View>
+  );
+};
 
-export default App
+export default App;
